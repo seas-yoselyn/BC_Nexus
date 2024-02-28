d8c4169745b46c1c6b96d3b864233f3c87099b78
eliasinul <eliasinul@gmail.com>
2024-02-28 05:59:19 -0800

    Updates

--- workflow/scripts/utilities.py
+++ workflow/scripts/utilities.py
@@ -8,22 +8,15 @@ def load_config(file_path):
         configs = yaml.safe_load(file)
     return configs
 
-def load_and_process_data(file_path, technologies):
-
-    # if "AnnualEmissions.csv" in file_path:
-    #     # Load data for emissions
-    #     print("Loading data for AnnualEmissions.csv")  # Add a print statement to check if data is being loaded
-    #     result_df = pd.read_csv(file_path)
-    #     all_years=sorted(set(result_df['YEAR']))
-    #     # return years, result_df
-    # else:
+def load_and_process_data(file_path, technologies, year_column, technology_column):
     df = pd.read_csv(file_path)
     yearly_summed_values = {tech: [] for tech in technologies}
-    all_years = sorted(set(df['YEAR']))
+    all_years = sorted(set(df[year_column]))
     grouped_data = {}
     for tech in technologies:
-        filtered_df = df[df['TECHNOLOGY'].str.startswith(tech)]
-        grouped = filtered_df.groupby('YEAR')['VALUE'].sum()
+        filtered_df = df[df[technology_column].str.startswith(tech)]
+        last_column = filtered_df.iloc[:, -1]
+        grouped = last_column.groupby(filtered_df[year_column]).sum()
         grouped_data[tech] = grouped.reindex(all_years, fill_value=0)
         yearly_summed_values[tech] = [grouped.get(year, 0) for year in all_years]
     result_df = pd.DataFrame(grouped_data)
