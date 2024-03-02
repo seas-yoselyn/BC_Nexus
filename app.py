11dbf1ec3ed73c20e6d43eccc8ab4a8d78159cf8
eliasinul <eliasinul@gmail.com>
2024-03-02 10:56:53 -0800

    Updates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
-import sys
-sys.path.append('workflow/scripts')
+# import sys
+# sys.path.append('workflow/scripts')
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
 import pandas as pd
-import utilities as utils
+from  workflow.scripts import utilities as utils
 import plotly.io as pio
 
 # Load configurations
