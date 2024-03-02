994831cf1b27a8176f6340a396d2206f5a2f6483
eliasinul <eliasinul@gmail.com>
2024-03-02 10:55:08 -0800

    Updates

---
+++ workflow/scripts/utils/utilities.py
@@ -0,0 +1,33 @@
+import yaml,os
+import pandas as pd
+
+# Function to Load User Configuration File
+def load_config(file_path):
+
+    with open(file_path, 'r') as file:
+        configs = yaml.safe_load(file)
+    return configs
+
+
+def load_and_process_data(file_path, technologies):
+    df = pd.read_csv(file_path)
+    year_col = 'YEAR'
+    technology_col = 'TECHNOLOGY'
+    if 'YEAR' not in df.columns or 'TECHNOLOGY' not in df.columns:
+        year_col = 'y'
+        technology_col = 't'
+    yearly_summed_values = {tech: [] for tech in technologies}
+    all_years = sorted(set(df[year_col]))
+    grouped_data = {}
+    for tech in technologies:
+        filtered_df = df[df[technology_col].str.startswith(tech)]
+        last_column = filtered_df.iloc[:, -1]
+        grouped = last_column.groupby(filtered_df[year_col]).sum()
+        grouped_data[tech] = grouped.reindex(all_years, fill_value=0)
+        yearly_summed_values[tech] = [grouped.get(year, 0) for year in all_years]
+    result_df = pd.DataFrame(grouped_data)
+
+    return all_years, result_df
+
+
+
